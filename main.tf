9a4c1b3c6ffe7b6dee45d74379387759ce3d3965
meerim1998 <school.meerim@gmail.com>
2024-07-06 15:13:48 -0400

    destroy

--- main.tf
+++ main.tf
@@ -1,14 +1,18 @@
+provider "aws" {
+  region = us-east-1
+}  
+
 module "eks" {
   source = "github.com/meerim1998/tf-modules//eks"
 
-  region                    = var.region
-  cluster_name              = var.cluster_name
-  subnet_ids                = var.subnet_ids
-  node_group_name           = var.node_group_name
-  node_count                = var.node_count
-  max_size                  = var.max_size
-  min_size                  = var.min_size
-  instance_type             = var.instance_type
-  disk_size_gb              = var.disk_size_gb
-  source_security_group_id  = var.source_security_group_id
+  region                    = "us-east-1"
+  cluster_name              = "my-eks-cluster"
+  subnet_ids                = ["subnet-0f798a592c6b368cd", "subnet-0a57d37ffce7cdb09"]
+  node_group_name           = "primary"
+  node_count                = 3
+  max_size                  = 5
+  min_size                  = 2
+  instance_type             = "t2.micro"
+  disk_size_gb              = 20
+  source_security_group_id  = "sg-00448e27b5c6f1f8e"
 }
